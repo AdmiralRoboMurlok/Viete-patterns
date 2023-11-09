03428adde9cc41e642421262e59a6f981b8481ec
Gerard <gerard.dziwinski@infotech.edu.pl>
2023-11-09 08:35:18 +0100

    Prace nad przyciskiem dodałem to, że symbole znakówpojawią się tylko kiedy delta wynosi więcej niż 0

--- main.py
+++ main.py
@@ -31,7 +31,8 @@ def EnterAll():
 def delta(a, b, c):
     global OblDelta
     OblDelta = (b*b)-4*a*c
-    updateLabel4()
+    label4 = tk.Label(root, text=("Delta is equal to ", OblDelta), font=('Times', 16))
+    label4.grid(row=4, column=0, columnspan=4, padx=5, pady=5)
 
 def x1orazx2(a, b, c):
     global x1addx2
@@ -62,18 +63,19 @@ def znaki():
 
 
 def updateLabel1and2():
-   label1.config(text=("x1 + x2 = ", x1addx2))
-   label2.config(text=("x1 * x2 = ", x1multiplyx2))
+   labelx1_x2.config(text=("x1 + x2 = ", x1addx2))
+   labelx1x2.config(text=("x1 * x2 = ", x1multiplyx2))
 
 def updateLabel3():
-   label3.config(text=("x1^2 + x2^2 = ", kwadra))
 
 
-def updateLabel4():
-    label4.config(text=("Delta is equal to ", OblDelta))
+   labelx12x22.config(text=("x1^2 + x2^2 = ", kwadra))
+
 
 def updateLabel5():
-    label5.config(text=znak)
+    labelPosOrNeg.config(text="Are the numbers positive or negative")
+
+    labelNumPosOrNeg.config(text=znak)
 
 def CountAll(a, b, c):
     delta(a, b, c)
@@ -107,25 +109,22 @@ entry3 = tk.Entry(root, width=40)
 entry3.grid(row=2, column=5, columnspan=6, padx=5, pady=5)
 
 
-label4 = tk.Label(root, text=("Delta is equal to ", OblDelta),font=('Times', 16))
-label4.grid(row=4, column=0, columnspan=4, padx=5, pady=5)
+labelx1_x2=tk.Label(root, text=("x1 + x2 = ", x1addx2), font=('Times', 16))
+labelx1_x2.grid(row=5, column=0, columnspan=4, padx=5, pady=5)
 
-label1=tk.Label(root, text=("x1 + x2 = ", x1addx2), font=('Times', 16))
-label1.grid(row=5, column=0, columnspan=4, padx=5, pady=5)
-
-label2=tk.Label(root, text=("x1 * x2 = ", x1multiplyx2), font=('Times', 16))
-label2.grid(row=6, column=0, columnspan=4, padx=5, pady=5)
+labelx1x2=tk.Label(root, text=("x1 * x2 = ", x1multiplyx2), font=('Times', 16))
+labelx1x2.grid(row=6, column=0, columnspan=4, padx=5, pady=5)
 
 button4 = tk.Button(root, text="Count", width=12, height=2, command=lambda: CountAll(a, b, c))
 button4.grid(row=4, column=12, columnspan=4, padx=5, pady=5)
 
-label3 = tk.Label(root, text=("x1^2 + x2^2 = ", kwadra), font=('Times', 16))
-label3.grid(row=7, column=0, columnspan=4, padx=5, pady=5)
+labelx12x22 = tk.Label(root, text=("x1^2 + x2^2 = ", kwadra), font=('Times', 16))
+labelx12x22.grid(row=7, column=0, columnspan=4, padx=5, pady=5)
 
-label6 = tk.Label(root, text="Are the numbers positive or negative", font=('Times', 16))
-label6.grid(row=8, column=1, columnspan=4, padx=5, pady=5)
+labelPosOrNeg = tk.Label(root, text="", font=('Times', 16))
+labelPosOrNeg.grid(row=8, column=1, columnspan=4, padx=5, pady=5)
 
-label5 = tk.Label(root, text=znak, font=('Times', 16))
-label5.grid(row=9, column=1, columnspan=4, padx=5, pady=5)
+labelNumPosOrNeg = tk.Label(root, text=znak, font=('Times', 16))
+labelNumPosOrNeg.grid(row=9, column=1, columnspan=4, padx=5, pady=5)
 
 root.mainloop()
